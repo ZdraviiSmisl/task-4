e07a0d66e65f774cb9c35b4537bc1680f84ab74f
ZdraviiSmisl <gormoshka123@gmail.com>
2020-02-25 14:59:07 +0300

    Updates

---
+++ precache-manifest.a6edf2190dbf154252d1800d8bf57f47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29573b84f6abead39fda6f2f36f88fbe",
+    "url": "/task-4/index.html"
+  },
+  {
+    "revision": "6bec056dd1dc81f0d60b",
+    "url": "/task-4/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c04934903485dd357a2e",
+    "url": "/task-4/static/js/2.aea9a4c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/task-4/static/js/2.aea9a4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bec056dd1dc81f0d60b",
+    "url": "/task-4/static/js/main.0749cd8f.chunk.js"
+  },
+  {
+    "revision": "2b4582658c023fcfe648",
+    "url": "/task-4/static/js/runtime-main.b1a6e1fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-4/static/media/logo.5d5d9eef.svg"
+  }
+]);

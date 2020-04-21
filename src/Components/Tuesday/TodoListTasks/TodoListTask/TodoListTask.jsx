a97581e1d6deb928f181902623911eb711bb17b3
ZdraviiSmisl <55047650+ZdraviiSmisl@users.noreply.github.com>
2020-04-21 22:43:18 +0300

    commit

--- src/Components/Tuesday/TodoListTasks/TodoListTask/TodoListTask.jsx
+++ src/Components/Tuesday/TodoListTasks/TodoListTask/TodoListTask.jsx
@@ -4,72 +4,72 @@ import style from './TodoListTask.module.css';
 import CustomizedTooltips from "../../../../OutsideComponents/Tooltip/Tooltipe";
 
 class TodoListTask extends React.Component {
-    state = {
-        editMode: false,
-    };
-    onIsDoneChanged = (e) => {
-        let status = e.currentTarget.checked ? 2 : 0
-        this.props.changeStatus(this.props.task.id, status);
-    };
-    activatedEditMode = () => {
-        this.setState({editMode: true})
-    };
-    deactivatedEditMode = () => {
-        this.setState({editMode: false})
-    };
-    onTitleChanged = (e) => {
-        this.props.changeTitle(this.props.task.id, e.currentTarget.value);
-    };
-    clickForDel = () => {
-        this.props.removeTask(this.props.task.id);
-    };
-    togglePriority = (e) => {
-        let priority = Number(e.target.value);
-        this.props.changePriority(this.props.task.id, priority)
-    };
+  state = {
+    editMode: false,
+  };
+  onIsDoneChanged = (e) => {
+    let status = e.currentTarget.checked ? 2 : 0
+    this.props.changeStatus(this.props.task.id, status);
+  };
+  activatedEditMode = () => {
+    this.setState({editMode: true})
+  };
+  deactivatedEditMode = () => {
+    this.setState({editMode: false})
+  };
+  onTitleChanged = (e) => {
+    this.props.changeTitle(this.props.task.id, e.currentTarget.value);
+  };
+  clickForDel = () => {
+    this.props.removeTask(this.props.task.id);
+  };
+  togglePriority = (e) => {
+    let priority = Number(e.target.value);
+    this.props.changePriority(this.props.task.id, priority)
+  };
 
-    render() {
-        let taskClass = this.props.task.isDone ? `${style.todoListTask} ${style.done}` : style.todoListTask;
-        let changePriorityClass = this.props.task.priority === 'low' ? style.low : this.props.task.priority === 'middle'
-            ? style.middle : this.props.task.priority === 'high' ? style.high : '';
+  render() {
+    let taskClass = this.props.task.isDone ? `${style.todoListTask} ${style.done}` : style.todoListTask;
+    let changePriorityClass = this.props.task.priority === 'low' ? style.low : this.props.task.priority === 'middle'
+      ? style.middle : this.props.task.priority === 'high' ? style.high : '';
 
-        return (
+    return (
 
-            <CustomizedTooltips created={this.props.task.created}
-                                updated={this.props.task.updated}
-                                finished={this.props.task.finished}>
-                <div className={style.wrap_task}>
-                    <div className={taskClass}>
-                        <input className={style.checkbox__class} type='checkbox'
-                               checked={this.props.task.status === 2}
-                               onChange={this.onIsDoneChanged}
-                        />
-                        {this.state.editMode ?
-                            <input className={style.input} autoFocus={true}
-                                   onBlur={this.deactivatedEditMode}
-                                   onChange={this.onTitleChanged}/>
-                            :
-                            <span onClick={this.activatedEditMode}
-                                  className={changePriorityClass}>{this.props.task.id} {this.props.task.title} </span>
-                        }
-                        <select className={style.select__class} value={this.props.task.priority}
-                                onChange={this.togglePriority}>
-                            <option value="2">high</option>
-                            <option value="0">low</option>
-                            <option value="1">middle</option>
-                        </select>
-                        <button className={style.delete__task} onClick={this.clickForDel}>X</button>
-                    </div>
-                </div>
-            </CustomizedTooltips>
+      <CustomizedTooltips created={this.props.task.created}
+                          updated={this.props.task.updated}
+                          finished={this.props.task.finished}>
+        <div className={style.wrap_task}>
+          <div className={taskClass}>
+            <input className={style.checkbox__class} type='checkbox'
+                   checked={this.props.task.status === 2}
+                   onChange={this.onIsDoneChanged}
+            />
+            {this.state.editMode ?
+              <input className={style.input} autoFocus={true}
+                     onBlur={this.deactivatedEditMode}
+                     onChange={this.onTitleChanged}/>
+              :
+              <span onClick={this.activatedEditMode}
+                    className={changePriorityClass}>{this.props.task.id} {this.props.task.title} </span>
+            }
+            <select className={style.select__class} value={this.props.task.priority}
+                    onChange={this.togglePriority}>
+              <option value="2">high</option>
+              <option value="0">low</option>
+              <option value="1">middle</option>
+            </select>
+            <button className={style.delete__task} onClick={this.clickForDel}>X</button>
+          </div>
+        </div>
+      </CustomizedTooltips>
 
-        );
-    }
+    );
+  }
 }
 
 export default TodoListTask;
 TodoListTask.propTypes = {
-    tasks: PropTypes.string,
-    isDone: PropTypes.bool,
-    priority: PropTypes.string
+  tasks: PropTypes.string,
+  isDone: PropTypes.bool,
+  priority: PropTypes.string
 };
--- src/Components/Wednesday/Wednesday.jsx
+++ src/Components/Wednesday/Wednesday.jsx
@@ -4,27 +4,34 @@ import {connect} from "react-redux";
 import {setStyle} from "../../Redux/reducers/settingsReducer";
 import axios from 'axios';
 
+const DARK_THEME = 'DARK_THEME';
+const LIGHT_THEME = 'LIGHT_THEME';
+const GREEN_THEME = 'GREEN_THEME';
 
-const Wednesday = (props) => {
-  const DARK_THEME = 'DARK_THEME';
-  const LIGHT_THEME = 'LIGHT_THEME';
-  const GREEN_THEME = 'GREEN_THEME';
+class Wednesday extends React.Component {
 
-
-  let status = false;
-  const toggleCheckbox = (e) => {
-    status = e.currentTarget.checked;
+  state = {
+    status: false
   }
 
-  const requestToServer = (status) => {
-   return  axios.post(`https://neko-cafe-back.herokuapp.com/auth/test`,
+
+  toggleCheckbox = (e) => {
+    let status = e.currentTarget.checked;
+    this.setState({
+      status: status
+    })
+  }
+
+  requestToServer = (status) => {
+    return axios.post(`https://neko-cafe-back.herokuapp.com/auth/test`,
       {success: status}
     )
+
   }
 
-  const tryCatch = async () => {
+  tryCatch = async () => {
     try {
-      const response = await requestToServer(status);
+      const response = await this.requestToServer(this.state.status);
       console.log('answer:', response.data);
       return response;
     } catch (e) {
@@ -34,42 +41,48 @@ const Wednesday = (props) => {
   }
 
 
-  const setTheme = (e) => {
+  setTheme = (e) => {
     let selectedTheme;
     if (e.currentTarget.checked) {
       selectedTheme = e.currentTarget.value;
     }
-    props.setStyle(selectedTheme);
+    this.props.setStyle(selectedTheme);
   }
-  return (
-    <div className={`${style.wrap__inputRadio} ${style[props.theme]}`}>
-      <label>Dark Theme <input name='them'
-                               type='radio'
-                               value={DARK_THEME}
-                               checked={props.theme === DARK_THEME}
-                               onChange={setTheme}/></label>
-      <label>Light Theme<input name='them'
-                               type='radio'
-                               value={LIGHT_THEME}
-                               checked={props.theme === LIGHT_THEME}
-                               onChange={setTheme}/></label>
-      <label>Green Theme<input name='them'
-                               type='radio'
-                               value={GREEN_THEME}
-                               checked={props.theme === GREEN_THEME}
-                               onChange={setTheme}/></label>
-      <input type='checkbox' onChange={toggleCheckbox}/>
-      <button onClick={tryCatch}>Send</button>
-    </div>
 
-  )
+  render() {
+
+
+    return (
+      <div className={`${style.wrap__inputRadio} ${style[this.props.theme]}`}>
+        <label>Dark Theme <input name='them'
+                                 type='radio'
+                                 value={DARK_THEME}
+                                 checked={this.props.theme === DARK_THEME}
+                                 onChange={this.setTheme}/></label>
+        <label>Light Theme<input name='them'
+                                 type='radio'
+                                 value={LIGHT_THEME}
+                                 checked={this.props.theme === LIGHT_THEME}
+                                 onChange={this.setTheme}/></label>
+        <label>Green Theme<input name='them'
+                                 type='radio'
+                                 value={GREEN_THEME}
+                                 checked={this.props.theme === GREEN_THEME}
+                                 onChange={this.setTheme}/></label>
+        <input type='checkbox' checked={this.state.status} onChange={this.toggleCheckbox}/>
+        <button onClick={this.tryCatch}>Send</button>
+      </div>
+
+    )
+  }
 }
 
-let mapStateToProps = ({settings}) => {
+const mapStateToProps = ({settings}) => {
   return {
     theme: settings.theme
   }
 }
+
 
 export default connect(mapStateToProps, {setStyle})(Wednesday);
 
